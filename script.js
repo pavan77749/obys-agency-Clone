b4179758201c04d2dcbb9cfdb88ce84b8ec80201
Pavan Gupta <pavangpay05@gmail.com>
2024-11-03 21:46:15 +0530

    completed with loader

---
+++ script.js
@@ -0,0 +1,39 @@
+var t1 = gsap.timeline();
+t1.from(".line h1", {
+  y: 100,
+  stagger: 0.5,
+  duration: 0.6,
+  delay: 0.5,
+});
+t1.from("#line1-part1 .line h2", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 45);
+  },
+});
+t1.to('.line h2 ',{
+    animationName:"anime",
+    opacity:1
+})
+t1.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 3.5,
+});
+t1.from("#page1", {
+  delay: 0.2,
+  y: 1600,
+  opacity: 0,
+  ease: Power4,
+});
+t1.to("#loader", {
+  display: "none",
+});
